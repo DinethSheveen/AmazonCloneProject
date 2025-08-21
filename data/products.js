6ea93509f3a70f933f34179a446ca51c592c3643
DinethSheveen <fdineth4000@gmail.com>
2025-08-21 13:01:01 +0100

    Added a product id to identify em uniquely

--- data/products.js
+++ data/products.js
@@ -1,6 +1,6 @@
 const products = [
   {
-    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     productImage: "images/products/athletic-cotton-socks-6-pairs.jpg",
     productName: "Black and Gray Athletic Cotton Socks - 6 Pairs",
     rating: {
@@ -15,7 +15,7 @@ const products = [
     ]
   },
   {
-    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
     productImage: "images/products/intermediate-composite-basketball.jpg",
     productName: "Intermediate Size Basketball",
     rating: {
@@ -29,7 +29,7 @@ const products = [
     ]
   },
   {
-    id: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
+    productId: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
     productImage: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
     productName: "Adults Plain Cotton T-Shirt - 2 Pack",
     rating: {
@@ -46,7 +46,7 @@ const products = [
     sizeChartLink: "/clothing-size-chart.png"
   },
   {
-    id: "54e0eccd-8f36-462b-b68a-8182611d9add",
+    productId: "54e0eccd-8f36-462b-b68a-8182611d9add",
     productImage: "images/products/black-2-slot-toaster.jpg",
     productName: "2 Slot Toaster - Black",
     rating: {
@@ -61,7 +61,7 @@ const products = [
     ]
   },
   {
-    id: "3ebe75dc-64d2-4137-8860-1f5a963e534b",
+    productId: "3ebe75dc-64d2-4137-8860-1f5a963e534b",
     productImage: "images/products/6-piece-white-dinner-plate-set.jpg",
     productName: "6 Piece White Dinner Plate Set",
     rating: {
@@ -76,7 +76,7 @@ const products = [
     ]
   },
   {
-    id: "8c9c52b5-5a19-4bcb-a5d1-158a74287c53",
+    productId: "8c9c52b5-5a19-4bcb-a5d1-158a74287c53",
     productImage: "images/products/6-piece-non-stick-baking-set.webp",
     productName: "6-Piece Nonstick, Carbon Steel Oven Bakeware Baking Set",
     rating: {
@@ -90,7 +90,7 @@ const products = [
     ]
   },
   {
-    id: "dd82ca78-a18b-4e2a-9250-31e67412f98d",
+    productId: "dd82ca78-a18b-4e2a-9250-31e67412f98d",
     productImage: "images/products/plain-hooded-fleece-sweatshirt-yellow.jpg",
     productName: "Plain Hooded Fleece Sweatshirt",
     rating: {
@@ -105,7 +105,7 @@ const products = [
     ]
   },
   {
-    id: "77919bbe-0e56-475b-adde-4f24dfed3a04",
+    productId: "77919bbe-0e56-475b-adde-4f24dfed3a04",
     productImage: "images/products/luxury-tower-set-6-piece.jpg",
     productName: "Luxury Towel Set - Graphite Gray",
     rating: {
@@ -122,7 +122,7 @@ const products = [
     ]
   },
   {
-    id: "3fdfe8d6-9a15-4979-b459-585b0d0545b9",
+    productId: "3fdfe8d6-9a15-4979-b459-585b0d0545b9",
     productImage: "images/products/liquid-laundry-detergent-plain.jpg",
     productName: "Liquid Laundry Detergent, 110 Loads, 82.5 Fl Oz",
     rating: {
@@ -136,7 +136,7 @@ const products = [
     ]
   },
   {
-    id: "58b4fc92-e98c-42aa-8c55-b6b79996769a",
+    productId: "58b4fc92-e98c-42aa-8c55-b6b79996769a",
     productImage: "images/products/knit-athletic-sneakers-gray.jpg",
     productName: "Waterproof Knit Athletic Sneakers - Gray",
     rating: {
@@ -151,7 +151,7 @@ const products = [
     ]
   },
   {
-    id: "5968897c-4d27-4872-89f6-5bcb052746d7",
+    productId: "5968897c-4d27-4872-89f6-5bcb052746d7",
     productImage: "images/products/women-chiffon-beachwear-coverup-black.jpg",
     productName: "Women's Chiffon Beachwear Cover Up - Black",
     rating: {
@@ -170,7 +170,7 @@ const products = [
     sizeChartLink: "/clothing-size-chart.png"
   },
   {
-    id: "aad29d11-ea98-41ee-9285-b916638cac4a",
+    productId: "aad29d11-ea98-41ee-9285-b916638cac4a",
     productImage: "images/products/round-sunglasses-black.jpg",
     productName: "Round Sunglasses",
     rating: {
@@ -184,7 +184,7 @@ const products = [
     ]
   },
   {
-    id: "04701903-bc79-49c6-bc11-1af7e3651358",
+    productId: "04701903-bc79-49c6-bc11-1af7e3651358",
     productImage: "images/products/women-beach-sandals.jpg",
     productName: "Women's Two Strap Buckle Sandals - Tan",
     rating: {
@@ -201,7 +201,7 @@ const products = [
     ]
   },
   {
-    id: "901eb2ca-386d-432e-82f0-6fb1ee7bf969",
+    productId: "901eb2ca-386d-432e-82f0-6fb1ee7bf969",
     productImage: "images/products/blackout-curtain-set-beige.webp",
     productName: "Blackout Curtains Set 4-Pack - Beige",
     rating: {
@@ -216,7 +216,7 @@ const products = [
     ]
   },
   {
-    id: "82bb68d7-ebc9-476a-989c-c78a40ee5cd9",
+    productId: "82bb68d7-ebc9-476a-989c-c78a40ee5cd9",
     productImage: "images/products/men-slim-fit-summer-shorts-gray.jpg",
     productName: "Men's Slim-Fit Summer Shorts",
     rating: {
@@ -231,7 +231,7 @@ const products = [
     ]
   },
   {
-    id: "c2a82c5e-aff4-435f-9975-517cfaba2ece",
+    productId: "c2a82c5e-aff4-435f-9975-517cfaba2ece",
     productImage: "images/products/electric-glass-and-steel-hot-water-kettle.webp",
     productName: "Electric Glass and Steel Hot Tea Water Kettle - 1.7-Liter",
     rating: {
@@ -246,7 +246,7 @@ const products = [
     ]
   },
   {
-    id: "6b07d4e7-f540-454e-8a1e-363f25dbae7d",
+    productId: "6b07d4e7-f540-454e-8a1e-363f25dbae7d",
     productImage: "images/products/facial-tissue-2-ply-18-boxes.jpg",
     productName: "Ultra Soft Tissue 2-Ply - 18 Box",
     rating: {
@@ -263,7 +263,7 @@ const products = [
     ]
   },
   {
-    id: "a82c6bac-3067-4e68-a5ba-d827ac0be010",
+    productId: "a82c6bac-3067-4e68-a5ba-d827ac0be010",
     productImage: "images/products/straw-sunhat.webp",
     productName: "Straw Lifeguard Sun Hat",
     rating: {
@@ -279,7 +279,7 @@ const products = [
     ]
   },
   {
-    id: "e4f64a65-1377-42bc-89a5-e572d19252e2",
+    productId: "e4f64a65-1377-42bc-89a5-e572d19252e2",
     productImage: "images/products/sky-flower-stud-earrings.webp",
     productName: "Sterling Silver Sky Flower Stud Earrings",
     rating: {
@@ -294,7 +294,7 @@ const products = [
     ]
   },
   {
-    id: "b0f17cc5-8b40-4ca5-9142-b61fe3d98c85",
+    productId: "b0f17cc5-8b40-4ca5-9142-b61fe3d98c85",
     productImage: "images/products/women-stretch-popover-hoodie-black.jpg",
     productName: "Women's Stretch Popover Hoodie",
     rating: {
@@ -313,7 +313,7 @@ const products = [
     sizeChartLink: "/clothing-size-chart.png"
   },
   {
-    id: "a93a101d-79ef-4cf3-a6cf-6dbe532a1b4a",
+    productId: "a93a101d-79ef-4cf3-a6cf-6dbe532a1b4a",
     productImage: "images/products/bathroom-rug.jpg",
     productName: "Bathroom Bath Rug Mat 20 x 31 Inch - Grey",
     rating: {
@@ -328,7 +328,7 @@ const products = [
     ]
   },
   {
-    id: "4f4fbcc2-4e72-45cc-935c-9e13d79cc57f",
+    productId: "4f4fbcc2-4e72-45cc-935c-9e13d79cc57f",
     productImage: "images/products/women-knit-ballet-flat-black.jpg",
     productName: "Women's Knit Ballet Flat",
     rating: {
@@ -344,7 +344,7 @@ const products = [
     ]
   },
   {
-    id: "8b5a2ee1-6055-422a-a666-b34ba28b76d4",
+    productId: "8b5a2ee1-6055-422a-a666-b34ba28b76d4",
     productImage: "images/products/men-golf-polo-t-shirt-blue.jpg",
     productName: "Men's Regular-Fit Quick-Dry Golf Polo Shirt",
     rating: {
@@ -362,7 +362,7 @@ const products = [
     sizeChartLink: "/clothing-size-chart.png"
   },
   {
-    id: "b86ddc8b-3501-4b17-9889-a3bad6fb585f",
+    productId: "b86ddc8b-3501-4b17-9889-a3bad6fb585f",
     productImage: "images/products/trash-can-with-foot-pedal-50-liter.jpg",
     productName: "Trash Can with Foot Pedal - Brushed Stainless Steel",
     rating: {
@@ -378,7 +378,7 @@ const products = [
     ]
   },
   {
-    id: "19c6a64a-5463-4d45-9af8-e41140a4100c",
+    productId: "19c6a64a-5463-4d45-9af8-e41140a4100c",
     productImage: "images/products/duvet-cover-set-blue-twin.jpg",
     productName: "Duvet Cover Set with Zipper Closure",
     rating: {
@@ -395,7 +395,7 @@ const products = [
     ]
   },
   {
-    id: "d2785924-743d-49b3-8f03-ec258e640503",
+    productId: "d2785924-743d-49b3-8f03-ec258e640503",
     productImage: "images/products/women-chunky-beanie-gray.webp",
     productName: "Women's Chunky Cable Beanie - Gray",
     rating: {
@@ -413,7 +413,7 @@ const products = [
     ]
   },
   {
-    id: "ee1f7c56-f977-40a4-9642-12ba5072e2b0",
+    productId: "ee1f7c56-f977-40a4-9642-12ba5072e2b0",
     productImage: "images/products/men-chino-pants-beige.jpg",
     productName: "Men's Classic-fit Pleated Chino Pants",
     rating: {
@@ -428,7 +428,7 @@ const products = [
     ]
   },
   {
-    id: "1c079479-8586-494f-ab53-219325432536",
+    productId: "1c079479-8586-494f-ab53-219325432536",
     productImage: "images/products/men-athletic-shoes-green.jpg",
     productName: "Men's Athletic Sneaker",
     rating: {
@@ -444,7 +444,7 @@ const products = [
     ]
   },
   {
-    id: "4df68c27-fd59-4a6a-bbd1-e754ddb6d53c",
+    productId: "4df68c27-fd59-4a6a-bbd1-e754ddb6d53c",
     productImage: "images/products/men-navigator-sunglasses-brown.jpg",
     productName: "Men's Navigator Sunglasses Pilot",
     rating: {
@@ -460,7 +460,7 @@ const products = [
     ]
   },
   {
-    id: "4e37dd03-3b23-4bc6-9ff8-44e112a92c64",
+    productId: "4e37dd03-3b23-4bc6-9ff8-44e112a92c64",
     productImage: "images/products/non-stick-cooking-set-15-pieces.webp",
     productName: "Non-Stick Cookware Set, Pots, Pans and Utensils - 15 Pieces",
     rating: {
@@ -474,7 +474,7 @@ const products = [
     ]
   },
   {
-    id: "a434b69f-1bc1-482d-9ce7-cd7f4a66ce8d",
+    productId: "a434b69f-1bc1-482d-9ce7-cd7f4a66ce8d",
     productImage: "images/products/vanity-mirror-silver.jpg",
     productName: "Vanity Mirror with Heavy Base - Chrome",
     rating: {
@@ -490,7 +490,7 @@ const products = [
     ]
   },
   {
-    id: "a45cfa0a-66d6-4dc7-9475-e2b01595f7d7",
+    productId: "a45cfa0a-66d6-4dc7-9475-e2b01595f7d7",
     productImage: "images/products/women-french-terry-fleece-jogger-camo.jpg",
     productName: "Women's Fleece Jogger Sweatpant",
     rating: {
@@ -507,7 +507,7 @@ const products = [
     ]
   },
   {
-    id: "d339adf3-e004-4c20-a120-40e8874c66cb",
+    productId: "d339adf3-e004-4c20-a120-40e8874c66cb",
     productImage: "images/products/double-elongated-twist-french-wire-earrings.webp",
     productName: "Double Oval Twist French Wire Earrings - Gold",
     rating: {
@@ -521,7 +521,7 @@ const products = [
     ]
   },
   {
-    id: "d37a651a-d501-483b-aae6-a9659b0757a0",
+    productId: "d37a651a-d501-483b-aae6-a9659b0757a0",
     productImage: "images/products/round-airtight-food-storage-containers.jpg",
     productName: "Round Airtight Food Storage Containers - 5 Piece",
     rating: {
@@ -536,7 +536,7 @@ const products = [
     ]
   },
   {
-    id: "0d7f9afa-2efe-4fd9-b0fd-ba5663e0a524",
+    productId: "0d7f9afa-2efe-4fd9-b0fd-ba5663e0a524",
     productImage: "images/products/coffeemaker-with-glass-carafe-black.jpg",
     productName: "Coffeemaker with Glass Carafe and Reusable Filter - 25 Oz, Black",
     rating: {
@@ -551,7 +551,7 @@ const products = [
     ]
   },
   {
-    id: "02e3a47e-dd68-467e-9f71-8bf6f723fdae",
+    productId: "02e3a47e-dd68-467e-9f71-8bf6f723fdae",
     productImage: "images/products/blackout-curtains-black.jpg",
     productName: "Blackout Curtains Set 42 x 84-Inch - Black, 2 Panels",
     rating: {
@@ -565,7 +565,7 @@ const products = [
     ]
   },
   {
-    id: "8a53b080-6d40-4a65-ab26-b24ecf700bce",
+    productId: "8a53b080-6d40-4a65-ab26-b24ecf700bce",
     productImage: "images/products/cotton-bath-towels-teal.webp",
     productName: "100% Cotton Bath Towels - 2 Pack, Light Teal",
     rating: {
@@ -580,7 +580,7 @@ const products = [
     ]
   },
   {
-    id: "10ed8504-57db-433c-b0a3-fc71a35c88a1",
+    productId: "10ed8504-57db-433c-b0a3-fc71a35c88a1",
     productImage: "images/products/knit-athletic-sneakers-pink.webp",
     productName: "Waterproof Knit Athletic Sneakers - Pink",
     rating: {
@@ -596,7 +596,7 @@ const products = [
     ]
   },
   {
-    id: "77a845b1-16ed-4eac-bdf9-5b591882113d",
+    productId: "77a845b1-16ed-4eac-bdf9-5b591882113d",
     productImage: "images/products/countertop-blender-64-oz.jpg",
     productName: "Countertop Blender - 64oz, 1400 Watts",
     rating: {
@@ -611,7 +611,7 @@ const products = [
     ]
   },
   {
-    id: "36c64692-677f-4f58-b5ec-0dc2cf109e27",
+    productId: "36c64692-677f-4f58-b5ec-0dc2cf109e27",
     productImage: "images/products/floral-mixing-bowl-set.jpg",
     productName: "10-Piece Mixing Bowl Set with Lids - Floral",
     rating: {
@@ -627,7 +627,7 @@ const products = [
     ]
   },
   {
-    id: "aaa65ef3-8d6f-4eb3-bc9b-a6ea49047d8f",
+    productId: "aaa65ef3-8d6f-4eb3-bc9b-a6ea49047d8f",
     productImage: "images/products/kitchen-paper-towels-30-pack.jpg",
     productName: "2-Ply Kitchen Paper Towels - 30 Pack",
     rating: {
@@ -642,7 +642,7 @@ const products = [
     ]
   },
   {
-    id: "bc2847e9-5323-403f-b7cf-57fde044a955",
+    productId: "bc2847e9-5323-403f-b7cf-57fde044a955",
     productImage: "images/products/men-cozy-fleece-zip-up-hoodie-red.jpg",
     productName: "Men's Full-Zip Hooded Fleece Sweatshirt",
     rating: {
