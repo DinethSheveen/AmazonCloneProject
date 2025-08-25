ff7656fc7428572de999b0d2928e25533d9a746b
DinethSheveen <fdineth4000@gmail.com>
2025-08-25 10:35:10 +0100

    Updating the cart HTML once the Delete link is clicked

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -4,97 +4,92 @@ import { formatCurrency } from "./generalFunctions/currencyFormatter.js";
 
 const cartElements = document.querySelector(".order-summary")
 
-function displayCart(){
-    cartElements.innerHTML = ""
-    cartProducts.forEach((cartProduct)=>{
-    let productId = cartProduct.productId;
-    let existingElement;
-    products.forEach((product)=>{
-        if(product.productId === productId){
-            existingElement = product
-        }
-    })
+cartProducts.forEach((cartProduct)=>{
+let productId = cartProduct.productId;
+let existingElement;
+products.forEach((product)=>{
+    if(product.productId === productId){
+        existingElement = product
+    }
+})
 
-        cartElements.innerHTML +=`
-            <div class="cart-item-container">
-                <div class="delivery-date">
-                Delivery date: Tuesday, June 21
+    cartElements.innerHTML +=`
+        <div class="cart-item-container cart-item-container-${existingElement.productId}">
+            <div class="delivery-date">
+            Delivery date: Tuesday, June 21
+            </div>
+
+            <div class="cart-item-details-grid">
+            <img class="product-image"
+                src="${existingElement.productImage}">
+
+                <div class="cart-item-details">
+                    <div class="product-name">
+                        ${existingElement.productName}
+                    </div>
+                    
+                    <div class="product-price">
+                        ${(formatCurrency(existingElement.priceInCents)*cartProduct.quantity).toFixed(2)}
+                    </div>
+                        
+                    <div class="product-quantity">
+                        <span>
+                            Quantity: <span class="quantity-label">${cartProduct.quantity}</span>
+                        </span>
+                        <span class="update-quantity-link link-primary">
+                            Update
+                        </span>
+                        <span class="delete-quantity-link link-primary" data-product-id = "${existingElement.productId}">
+                            Delete
+                        </span>
+                    </div>
                 </div>
 
-                <div class="cart-item-details-grid">
-                <img class="product-image"
-                    src="${existingElement.productImage}">
-
-                    <div class="cart-item-details">
-                        <div class="product-name">
-                            ${existingElement.productName}
-                        </div>
-                        
-                        <div class="product-price">
-                            ${(formatCurrency(existingElement.priceInCents)*cartProduct.quantity).toFixed(2)}
-                        </div>
-                            
-                        <div class="product-quantity">
-                            <span>
-                                Quantity: <span class="quantity-label">${cartProduct.quantity}</span>
-                            </span>
-                            <span class="update-quantity-link link-primary">
-                                Update
-                            </span>
-                            <span class="delete-quantity-link link-primary" data-product-id = "${existingElement.productId}">
-                                Delete
-                            </span>
+                <div class="delivery-options">
+                    <div class="delivery-options-title">
+                        Choose a delivery option:
+                    </div>
+                    <div class="delivery-option">
+                        <input type="radio" checked class="delivery-option-input" name="${existingElement.productId}">
+                        <div>
+                            <div class="delivery-option-date">
+                                Tuesday, June 21
+                            </div>
+                            <div class="delivery-option-price">
+                            FREE Shipping
+                            </div>
                         </div>
                     </div>
-
-                    <div class="delivery-options">
-                        <div class="delivery-options-title">
-                            Choose a delivery option:
-                        </div>
-                        <div class="delivery-option">
-                            <input type="radio" checked class="delivery-option-input" name="${existingElement.productId}">
-                            <div>
-                                <div class="delivery-option-date">
-                                    Tuesday, June 21
-                                </div>
-                                <div class="delivery-option-price">
-                                FREE Shipping
-                                </div>
+                    
+                    <div class="delivery-option">
+                        <input type="radio" class="delivery-option-input" name="${existingElement.productId}">
+                        <div>
+                            <div class="delivery-option-date">
+                                Wednesday, June 15
+                            </div>
+                            <div class="delivery-option-price">
+                            $4.99 - Shipping
                             </div>
                         </div>
-                        
-                        <div class="delivery-option">
-                            <input type="radio" class="delivery-option-input" name="${existingElement.productId}">
-                            <div>
-                                <div class="delivery-option-date">
-                                    Wednesday, June 15
-                                </div>
-                                <div class="delivery-option-price">
-                                $4.99 - Shipping
-                                </div>
+                    </div>
+                    
+                    <div class="delivery-option">
+                        <input type="radio" class="delivery-option-input"
+                            name="${existingElement.productId}">
+                        <div>
+                            <div class="delivery-option-date">
+                                Monday, June 13
                             </div>
-                        </div>
-                        
-                        <div class="delivery-option">
-                            <input type="radio" class="delivery-option-input"
-                                name="${existingElement.productId}">
-                            <div>
-                                <div class="delivery-option-date">
-                                    Monday, June 13
-                                </div>
-                                <div class="delivery-option-price">
-                                $9.99 - Shipping
-                                </div>
+                            <div class="delivery-option-price">
+                            $9.99 - Shipping
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        `    
-    })
-}
-displayCart()
-
+        </div>
+    `    
+})
 
 document.querySelectorAll(".delete-quantity-link").forEach((product)=>{
     product.addEventListener("click",()=>{
@@ -103,7 +98,8 @@ document.querySelectorAll(".delete-quantity-link").forEach((product)=>{
         if(userConfirmation){
             removeFromCart(productId)
         }
-        displayCart()
+        const containerProduct = document.querySelector(`.cart-item-container-${productId}`)
+        containerProduct.remove()        
     })
 })
 
