dc8620ddc47e81cf57806284c8d673863945a1fb
DinethSheveen <fdineth4000@gmail.com>
2025-09-01 08:58:18 +0100

    delivery.js to store the delivery details

---
+++ data/delivery.js
@@ -0,0 +1,17 @@
+export const deliveryOptions = [
+    {
+        deliveryId : "1",
+        deliveryDays : 7,
+        deliveryPriceInCents : 0
+    },
+    {
+        deliveryId : "2",
+        deliveryDays : 3,
+        deliveryPriceInCents : 499
+    },
+    {
+        deliveryId : "3",
+        deliveryDays : 1,
+        deliveryPriceInCents : 999
+    }
+]
