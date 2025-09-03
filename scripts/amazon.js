ba9eae6ee00ee91a022fad0840df6c5d1f252ebb
DinethSheveen <fdineth4000@gmail.com>
2025-09-03 13:00:45 +0100

    Generated the product rating count

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -22,7 +22,7 @@ products.forEach((product)=>{
                 <img class="product-rating-stars"
                 src="images/ratings/rating-${product.rating.stars*10}.png">
                 <div class="product-rating-count link-primary">
-                    87
+                    ${product.rating.count}
                 </div>
             </div>
 
