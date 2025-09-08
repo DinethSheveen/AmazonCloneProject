1af23186a82350c44472f0eea5df1721f4000d0c
DinethSheveen <fdineth4000@gmail.com>
2025-09-08 12:40:24 +0100

    Added a flaky test on addToCart function

--- jasmine-testing/data/cartTest.js
+++ jasmine-testing/data/cartTest.js
@@ -1,9 +1,16 @@
-import {cartProducts,addToCart} from "../../data/cart.js"
+import {cartProducts,addToCart,loadCartProductsFromStorage} from "../../data/cart.js"
 
 //TEST SUITE
 describe("TEST SUITE : CART RELATED TESTS",()=>{
     //TESTCASE
     it("Adding a new product to the cart",()=>{
+        //MOCKS - TO TEST A FLAKY TEST
+        spyOn(localStorage,"getItem").and.callFake(()=>{
+            return JSON.stringify([])
+        })
+        loadCartProductsFromStorage()
+        console.log(localStorage);
+        
         addToCart("e43638ce-6aa0-4b85-b27f-e1d07eb678c6",1)
         expect(cartProducts.length).toEqual(1)
     })
