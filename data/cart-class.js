7b36c34e233c606c0d69eee4b60036ee86e275bd
DinethSheveen <fdineth4000@gmail.com>
2025-09-08 23:11:39 +0100

    Added a class version of cart.js

---
+++ data/cart-class.js
@@ -0,0 +1,104 @@
+function Cart(localStorageKey){
+    const cart = {
+        cartProducts : undefined,
+
+        loadCartProductsFromStorage(){
+            this.cartProducts = JSON.parse(localStorage.getItem(localStorageKey))
+
+            if (!this.cartProducts){
+                this.cartProducts = [
+                    {
+                        productId : "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+                        quantity : 2,
+                        deliveryOptionId :"1"
+                    },
+                    {
+                        productId : "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+                        quantity : 1,
+                        deliveryOptionId : "2"
+                    }
+                ]
+            }
+        },
+
+        addToCart(id,productCount){
+            // A VARIABLE TO STORE THE DUPLICATE ELEMENTS, IF EXISTS
+            let existingProduct;
+
+            //CHECKING WHETHER THE PRODUCT ALREADY EXISTS IN THE CART
+            this.cartProducts.forEach((product)=>{
+                if(id === product.productId){
+                    existingProduct = product
+                }
+            })
+
+            //IF PRODUCT ALREADY EXISTS, INCREASING THE QUANTITY
+            if(existingProduct){
+                existingProduct.quantity += productCount;
+            }
+            else{
+                this.cartProducts.push({
+                    productId : id,
+                    quantity : productCount,
+                    deliveryOptionId : "1"
+                })
+            }
+            this.saveCart()
+        },
+
+        removeFromCart(id){
+            let newCart = [];
+
+            this.cartProducts.forEach((product)=>{
+                if(product.productId !== id){
+                    newCart.push(product)
+                }
+            })
+
+            this.cartProducts = newCart
+            renderPaymentSummary()
+            saveCart()  
+        },
+
+        //SAVING THE CART DATA TO LOCAL STORAGE
+        saveCart(){
+            localStorage.setItem(localStorageKey,JSON.stringify(this.cartProducts))
+        },
+
+        // A FUNCTION TO CHANGE THE SHIPPING OPTIONS
+
+        updateShippingDate(productId,deliveryId){
+            let existingProduct;
+
+            this.cartProducts.forEach((cartProduct)=>{
+                if(cartProduct.productId === productId){
+                    existingProduct = cartProduct
+                }
+            })
+
+            existingProduct.deliveryOptionId = deliveryId;
+            saveCart()
+        },
+
+        updateCartTotal(element){
+            let cartCount = 0;
+
+            this.cartProducts.forEach((product)=>{
+                cartCount += product.quantity
+            })
+
+            element.innerHTML = cartCount
+        }
+    }
+
+    return cart
+}
+
+const cart = Cart("business-cart");
+
+cart.loadCartProductsFromStorage()
+
+console.log(cart);
+//cart.addToCart("54e0eccd-8f36-462b-b68a-8182611d9add",1)
+
+
