5cd9fd639abd7a555df1d6e2a7f59eba6c1e8db3
DinethSheveen <fdineth4000@gmail.com>
2025-09-08 23:42:36 +0100

    Updated the class-version with parameters

--- data/cart-class.js
+++ data/cart-class.js
@@ -1,108 +1,110 @@
-function Cart(localStorageKey){
-    const cart = {
-        cartProducts : undefined,
+class Cart{    
+    cartProducts = undefined;
 
-        loadCartProductsFromStorage(){
-            this.cartProducts = JSON.parse(localStorage.getItem(localStorageKey))
+    constructor(localStorageKey){
+        this.localStorageKey = localStorageKey
+    }
 
-            if (!this.cartProducts){
-                this.cartProducts = [
-                    {
-                        productId : "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-                        quantity : 2,
-                        deliveryOptionId :"1"
-                    },
-                    {
-                        productId : "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-                        quantity : 1,
-                        deliveryOptionId : "2"
-                    }
-                ]
-            }
-        },
+    loadCartProductsFromStorage(){
+        this.cartProducts = JSON.parse(localStorage.getItem(this.localStorageKey))
 
-        addToCart(id,productCount){
-            // A VARIABLE TO STORE THE DUPLICATE ELEMENTS, IF EXISTS
-            let existingProduct;
-
-            //CHECKING WHETHER THE PRODUCT ALREADY EXISTS IN THE CART
-            this.cartProducts.forEach((product)=>{
-                if(id === product.productId){
-                    existingProduct = product
+        if (!this.cartProducts){
+            this.cartProducts = [
+                {
+                    productId : "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+                    quantity : 2,
+                    deliveryOptionId :"1"
+                },
+                {
+                    productId : "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+                    quantity : 1,
+                    deliveryOptionId : "2"
                 }
-            })
-
-            //IF PRODUCT ALREADY EXISTS, INCREASING THE QUANTITY
-            if(existingProduct){
-                existingProduct.quantity += productCount;
-            }
-            else{
-                this.cartProducts.push({
-                    productId : id,
-                    quantity : productCount,
-                    deliveryOptionId : "1"
-                })
-            }
-            this.saveCart()
-        },
-
-        removeFromCart(id){
-            let newCart = [];
-
-            this.cartProducts.forEach((product)=>{
-                if(product.productId !== id){
-                    newCart.push(product)
-                }
-            })
-
-            this.cartProducts = newCart
-            renderPaymentSummary()
-            saveCart()  
-        },
-
-        //SAVING THE CART DATA TO LOCAL STORAGE
-        saveCart(){
-            localStorage.setItem(localStorageKey,JSON.stringify(this.cartProducts))
-        },
-
-        // A FUNCTION TO CHANGE THE SHIPPING OPTIONS
-
-        updateShippingDate(productId,deliveryId){
-            let existingProduct;
-
-            this.cartProducts.forEach((cartProduct)=>{
-                if(cartProduct.productId === productId){
-                    existingProduct = cartProduct
-                }
-            })
-
-            existingProduct.deliveryOptionId = deliveryId;
-            saveCart()
-        },
-
-        updateCartTotal(element){
-            let cartCount = 0;
-
-            this.cartProducts.forEach((product)=>{
-                cartCount += product.quantity
-            })
-
-            element.innerHTML = cartCount
+            ]
         }
     }
 
-    return cart
+    addToCart(id,productCount){
+        // A VARIABLE TO STORE THE DUPLICATE ELEMENTS, IF EXISTS
+        let existingProduct;
+
+        //CHECKING WHETHER THE PRODUCT ALREADY EXISTS IN THE CART
+        this.cartProducts.forEach((product)=>{
+            if(id === product.productId){
+                existingProduct = product
+            }
+        })
+
+        //IF PRODUCT ALREADY EXISTS, INCREASING THE QUANTITY
+        if(existingProduct){
+            existingProduct.quantity += productCount;
+        }
+        else{
+            this.cartProducts.push({
+                productId : id,
+                quantity : productCount,
+                deliveryOptionId : "1"
+            })
+        }
+        this.saveCart()
+    }
+
+    removeFromCart(id){
+        let newCart = [];
+
+        this.cartProducts.forEach((product)=>{
+            if(product.productId !== id){
+                newCart.push(product)
+            }
+        })
+
+        this.cartProducts = newCart
+        renderPaymentSummary()
+        saveCart()  
+    }
+
+    //SAVING THE CART DATA TO LOCAL STORAGE
+    saveCart(){
+        localStorage.setItem(this.localStorageKey,JSON.stringify(this.cartProducts))
+    }
+
+    // A FUNCTION TO CHANGE THE SHIPPING OPTIONS
+
+    updateShippingDate(productId,deliveryId){
+        let existingProduct;
+
+        this.cartProducts.forEach((cartProduct)=>{
+            if(cartProduct.productId === productId){
+                existingProduct = cartProduct
+            }
+        })
+
+        existingProduct.deliveryOptionId = deliveryId;
+        saveCart()
+    }
+
+    updateCartTotal(element){
+        let cartCount = 0;
+
+        this.cartProducts.forEach((product)=>{
+            cartCount += product.quantity
+        })
+
+        element.innerHTML = cartCount
+    }
 }
 
-const cart = Cart("cart-oop");
-cart.loadCartProductsFromStorage()
-cart.addToCart("54e0eccd-8f36-462b-b68a-8182611d9add",1)
 
-const businessCart = Cart("business-cart")
-businessCart.loadCartProductsFromStorage()
+const carts = new Cart("business-cart")
+carts.loadCartProductsFromStorage()
+carts.addToCart("54e0eccd-8f36-462b-b68a-8182611d9add",1)
 
-console.log(cart);
-console.log(businessCart);
+const cartss = new Cart("cart")
+cartss.loadCartProductsFromStorage()
+
+console.log(carts);
+console.log(cartss);
+
 
 //cart.addToCart("54e0eccd-8f36-462b-b68a-8182611d9add",1)
 
