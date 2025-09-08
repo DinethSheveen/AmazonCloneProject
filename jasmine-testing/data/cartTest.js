3753e7a487ff3575cee6a4ea46e1084e0d020637
DinethSheveen <fdineth4000@gmail.com>
2025-09-08 12:49:02 +0100

    addded more tests on adding new products to the cart

--- jasmine-testing/data/cartTest.js
+++ jasmine-testing/data/cartTest.js
@@ -2,16 +2,17 @@ import {cartProducts,addToCart,loadCartProductsFromStorage} from "../../data/car
 
 //TEST SUITE
 describe("TEST SUITE : CART RELATED TESTS",()=>{
-    //TESTCASE
+    //TESTCASE - 01
     it("Adding a new product to the cart",()=>{
         //MOCKS - TO TEST A FLAKY TEST
         spyOn(localStorage,"getItem").and.callFake(()=>{
             return JSON.stringify([])
         })
         loadCartProductsFromStorage()
-        console.log(localStorage);
         
         addToCart("e43638ce-6aa0-4b85-b27f-e1d07eb678c6",1)
         expect(cartProducts.length).toEqual(1)
+        expect(cartProducts[0].productId).toEqual("e43638ce-6aa0-4b85-b27f-e1d07eb678c6")
+        expect(cartProducts[0].quantity).toEqual(1)
     })
 })
